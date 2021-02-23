52af3a299a6749dc3540c2c49309a8a03a8ca783
Sidiney <sidineysilva1@gmail.com>
2021-02-23 11:07:14 -0300

    Lazy e Suspense no componente de estatisticas

--- src/containers/UserStats/index.tsx
+++ src/containers/UserStats/index.tsx
@@ -2,10 +2,13 @@ import { STATS_GET } from "api";
 import ErrorMessage from "components/ErrorMessage";
 import Head from "components/Head";
 import Loading from "components/Loading";
-import UserStatsGraphs from "components/UserStatsGraphs";
 import useFetch from "hooks/useFetch";
-import { useEffect } from "react";
+import { lazy, Suspense, useEffect } from "react";
 import { UserStatsData } from "types/components/UserStatsGraphs";
+
+const UserStatsGraphs = lazy(
+  () => import("./../../components/UserStatsGraphs/index")
+);
 
 function UserStats() {
   const { data, error, loading, request } = useFetch<UserStatsData[]>();
@@ -25,10 +28,10 @@ function UserStats() {
 
   if (data)
     return (
-      <div>
+      <Suspense fallback={<Loading />}>
         <Head title="Estatísticas" />
         <UserStatsGraphs data={data} />
-      </div>
+      </Suspense>
     );
   else return null;
 }
